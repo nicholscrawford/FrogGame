Added background, fixed color rotation, added more map.

rotate_colors.py
@@ -14,7 +14,7 @@ def convert_color_space(input_folder, output_folder, target_color, target_color_
     files = os.listdir(input_folder)
 
     for file_name in files:
-        if file_name.endswith(".png"):
+        if file_name.endswith(".png") and "Frog" in file_name:
             # Read the image with alpha channel
             img_path = os.path.join(input_folder, file_name)
             img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
@@ -29,11 +29,27 @@ def convert_color_space(input_folder, output_folder, target_color, target_color_
                 # Convert color space to HLS (Hue, Lightness, Saturation)
                 img_hls = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2HLS)
 
-                # Set new hue value based on target color
-                hls_target = np.array(
-                    cv2.cvtColor(np.uint8([[target_color]]), cv2.COLOR_RGB2HLS)
+                # Extract individual channels
+                h, l, s = cv2.split(img_hls)
+
+                # Convert target color to HLS
+                hls_target = cv2.cvtColor(
+                    np.uint8([[target_color]]), cv2.COLOR_RGB2HLS
                 )[0][0]
-                img_hls[:, :, 0] = hls_target[0]
+                target_hue = hls_target[0]
+
+                # Adjust hue and scale lightness
+                hue_diff = 44 - target_hue
+                h = np.mod(h + hue_diff, 180).astype(
+                    np.uint8
+                )  # Ensure hue stays within [0, 180]
+                l = np.clip(l * (hls_target[1] / 105), 0, 255).astype(
+                    np.uint8
+                )  # Scale lightness
+                s = np.clip(s * (hls_target[2] / 255), 0, 255).astype(np.uint8)
+
+                # Merge channels
+                img_hls = cv2.merge((h, l, s))
 
                 # Convert back to RGB
                 converted_img_rgb = cv2.cvtColor(img_hls, cv2.COLOR_HLS2RGB)
@@ -48,9 +64,6 @@ def convert_color_space(input_folder, output_folder, target_color, target_color_
                 # Write the converted image to the output folder
                 output_path = os.path.join(output_folder, file_name)
                 cv2.imwrite(output_path, converted_img_rgba)
-                # print(
-                #     f"Converted {file_name} to {target_color} and saved as {output_path}"
-                # )
 
 
 # Input and output folders